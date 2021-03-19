cf4f4adb053d1f1d681d1dc9674c2095460a5919
Ihor Kolopatin <ikolopatin@gmail.com>
2021-03-19 19:15:36 +0200

    Friday

--- Chapter3/Chapter3/Program.cs
+++ Chapter3/Chapter3/Program.cs
@@ -1,12 +1,174 @@
 ﻿using System;
-
+using static System.Console;
+using MyLib;
 namespace Chapter3
 {
     class Program
     {
+        struct User
+        {
+            public string name;
+            public int age;
+            public User(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Name: {name}  Age: {age}");
+            }
+        }
+        struct State
+        {
+            public int x;
+            public int y;
+            public Country country;
+        }
+        class Country
+        {
+            public int x;
+            public int y;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //Person p=new Person();
+            //Person s = new Person("Tom", 21);
+            //p.SetInfo();
+            //p.GetInfo();
+            //s.GetInfo();
+            //User u = new User { name = "Tom", age = 89 };
+            //u.DisplayInfo();
+            //User a = new User("Alex", 19);
+            //a.DisplayInfo();
+            //User b = new User();
+            //b.DisplayInfo();
+
+            //
+            State state1 = new State();
+            State state2 = new State();
+
+            state2.country = new Country();
+            state2.country.x = 5;
+            state1 = state2;
+            state2.country.x = 8; // теперь и state1.country.x=8, так как state1.country и state2.country
+                                  // указывают на один объект в хипе
+            Console.WriteLine(state1.country.x); // 8
+            Console.WriteLine(state2.country.x); // 8
+            //
+                Person p = new Person { name = "Tom", age = 23 };
+                ChangePerson(ref p);
+
+                WriteLine(p.name); // Bill
+                WriteLine(p.age); // 45
+            //
+            Human j = new Human { name = "Jack"};
+            Console.WriteLine("Please, tell me your age");
+            j.Age = Int32.Parse(ReadLine());
+            WriteLine($"Name: {j.name} Age: {j.Age}");
+
+            //
+            Access acc = new Access();
+            acc.internalMethod();
+            acc.protectedInternalMethod();
+            acc.publicMethod();
+            //
+             int v = Int32.Parse(ReadLine());
+            IncrementVal(v);
+            IncrementVal(ref v);
+            
+            Console.WriteLine(v);
+            Console.WriteLine("sum");
+            decimal sum = Decimal.Parse(ReadLine());
+            Console.WriteLine("rate");
+            decimal rate = Decimal.Parse(ReadLine());
+            Console.WriteLine("period");
+            int period = Int32.Parse(ReadLine());
+            Console.WriteLine(Account.GetSum(sum,rate,period));
+
+            //
+            Uther uth = new Uther("rob",67);
+            Console.WriteLine(uth.Age);
+            //
+            Strana ss1 = new Strana();
+            ss1.Area = 120000;
+            ss1.Population = 1000000;
+            Strana ss2 = new Strana();
+            ss2.Area = 150000;
+            ss2.Population = 750000;
+            Strana state3 = ss1 + ss2;
+            bool isGreater = ss1 > ss2;
+            if (isGreater == true)
+            {
+                Console.WriteLine($"Greater is {ss1}");
+            }
+            else if (isGreater == false)
+            {
+                Console.WriteLine($"Greater is {ss2}");
+            }
+            
+            Console.WriteLine($"New Country population={state3.Population} New Country Area={state3.Area}");
+
+            //
+            Bread bread = new Bread { Weight = 80 };
+            Butter butter = new Butter { Weight = 20 };
+            Sandwich sandwich = bread + butter;
+            Console.WriteLine(sandwich.Weight);
+
+            Read();
+        }
+        static void ChangePerson(ref Person person)
+        {
+            // сработает
+            person.name = "Alice";
+            // сработает
+            person = new Person { name = "Bill", age = 45 };
+
+        }
+        static void IncrementVal(ref int val)
+        {
+            val++;
+            Console.WriteLine(val);
+        }
+        static void IncrementVal(int val)
+        {
+            val++;
+            Console.WriteLine(val);
+        }
+
+        readonly struct Uther
+        {
+            public readonly string Name { get; } // указывать readonly необязательно
+            public int Age { get; } // свойство только для чтения
+            public Uther(string name, int age)
+            {
+                this.Name = name;
+                this.Age = age;
+            }
+        }
+
+        class Bread
+        {
+            public int Weight { get; set; } // масса
+            public static Sandwich operator +(Bread br, Butter b)
+            {
+                return new Sandwich { Weight = br.Weight + b.Weight };
+            }
+        }
+
+        // масло
+        class Butter
+        {
+            public int Weight { get; set; } // масса
+        }
+
+        // бутерброт
+        class Sandwich
+        {
+            
+            public int Weight { get; set; } // масса
+            
         }
     }
 }
