63f13885051edd353d428cd9571c4d544c93dff0
Ihor Kolopatin <ikolopatin@gmail.com>
2021-03-22 22:00:52 +0200

    3/22 part2

---
+++ Chapter3Part2/Chapter3Part2/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter3Part2
+{
+    class Employee:Person
+    {
+        string company;
+        public Employee(string name,int age,string company) : base(name, age)
+        {
+            this.company = company;
+        }
+        public string Company
+        {
+            get
+            {
+                return this.company;
+            }
+            set
+            {
+                this.company = value;
+            }
+        }
+    }
+}
---
+++ Chapter3Part2/Chapter3Part2/Human.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter3Part2
+{
+    public abstract class Human
+    {
+        public abstract string Name { get; set; }
+        public int Age { get; set; }
+        public string Social { get; set; }
+        public Human(int age,string social)
+        {
+    
+            Age = age;
+            Social = social;
+        }
+        public abstract void Display();
+    }
+}
