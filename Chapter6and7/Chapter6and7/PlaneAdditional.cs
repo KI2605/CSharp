4672ecb215051fb509eae146963183e6880a4bfa
Ihor Kolopatin <ikolopatin@gmail.com>
2021-03-25 18:41:56 +0200

    here is chapter 6 and 7 untill the deconsructors

---
+++ Chapter6and7/Chapter6and7/PlaneAdditional.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter6and7
+{
+    public partial class Plane
+    {
+        partial void DoSomethingElse()
+        {
+            Console.WriteLine("I am chilling");
+        }
+    }
+}
---
+++ Chapter6and7/Chapter6and7/PlaneBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter6and7
+{
+    public partial class Plane
+    {
+        partial void DoSomethingElse();
+        public void Move()
+        {
+            DoSomethingElse();
+            Console.WriteLine("I am a plane- and I am moving");
+        }
+    }
+}
