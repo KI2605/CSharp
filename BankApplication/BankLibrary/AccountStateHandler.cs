5f4d9bc945f80e2d8367c769d7b805edf61883f0
Ihor Kolopatin <ikolopatin@gmail.com>
2021-03-29 18:46:40 +0300

    Chapter 8

---
+++ BankApplication/BankLibrary/AccountStateHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public delegate void AccountStateHandler(object sender, AccountEvent e);
+    public class AccountEvent
+    {
+        //message
+        public string Message { get; set; }
+        //sum changes
+        public decimal Sum { get; private set; }
+        public AccountEvent(string mes,decimal sum)
+        {
+            Message = mes;
+            Sum = sum;
+        }
+    }
+}
---
+++ BankApplication/BankLibrary/DemandAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    //класс, который будет представлять счет до востребования
+    class DemandAccount :Account
+    {
+        public DemandAccount(decimal sum, int percentage) : base(sum, percentage)
+        {
+
+        }
+        protected internal override void Open()
+        {
+            base.OnOpened(new AccountEvent($"Открыт новый счет до востребования! Id счета: {this.Id}", this.Sum));
+        }
+    }
+
+}
